f4cceef4c70b09b026e2f13a8fa767e27a85d71e
victorwillianpr <victorwillian6112@gmail.com>
2023-10-20 02:23:05 -0300

    CRUD-AspNetCoreMVC

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class mvcDemoDbContext : DbContext
+    {
+        public mvcDemoDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
